68cf9596c55946ac3cba11cda3141d78bad07975
radekzien <radoslaw.zien@outlook.com>
2025-11-12 16:18:23 +0000

    Increased Sample Pool; Created Array of filepath strings for ease of use

--- ProcessAudio.py
+++ ProcessAudio.py
@@ -1,10 +1,18 @@
 import librosa
 from scipy import signal
+
+SampleAudio = [
+    "SampleAudio/Classicals.de - Chopin - Albumleaf, B. 151.mp3",
+    "SampleAudio/Classicals.de - Chopin - Andantino 'Spring' B. 117.mp3",
+    "SampleAudio/Classicals.de - Chopin - Etude Op. 10 no. 12 in C minor 'Revolutionary'.mp3",
+    "SampleAudio/Classicals.de - Chopin - Fantasia Impromptu Op. posth. 66.mp3"
+]
+
 def ProcessAudio(filePath):
     #Load file and convert into frequency-time series
     audio_data,sr = librosa.load(filePath)
     print(f"Raw data: {audio_data.shape}")
-    print(sr)
+    print(f"Input Sample Rate: {sr}")
 
     #Calculate Wn value for filtering
     wn = 5000/(sr/2)
@@ -21,4 +29,4 @@ def ProcessAudio(filePath):
     return audio_data_resampled
 
 if __name__ == '__main__':
-    ProcessAudio("SampleAudio/Classicals.de - Chopin - Etude Op. 10 no. 12 in C minor 'Revolutionary'.mp3")
+    ProcessAudio(SampleAudio[0])
