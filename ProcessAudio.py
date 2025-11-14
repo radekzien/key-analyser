c4701410bb2f88320e89c34a248a8c5bc28617e2
radekzien <radoslaw.zien@outlook.com>
2025-11-14 12:26:31 +0000

    removed debug prints from ProcessAudio.py

--- ProcessAudio.py
+++ ProcessAudio.py
@@ -4,8 +4,6 @@ from scipy import signal
 def ProcessAudio(filePath):
     #Load file and convert into frequency-time series
     audio_data,sr = librosa.load(filePath)
-    print(f"Raw data: {audio_data.shape}")
-    print(f"Input Sample Rate: {sr}")
 
     #Calculate Wn value for filtering
     wn = 5000/(sr/2)
@@ -13,10 +11,8 @@ def ProcessAudio(filePath):
     
     #Apply filter
     audio_data_filtered = signal.lfilter(b, a, audio_data)
-    print(f"Filtered data: {audio_data_filtered.shape}")
 
     #Resample to 16kHz
     audio_data_resampled = librosa.resample(audio_data_filtered, orig_sr=sr, target_sr=16000)
-    print(f"Resampled data: {audio_data_resampled.shape}")
     
     return audio_data_resampled
