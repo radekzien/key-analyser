f068b55970b9b9282417ad1a886b948ad87f0c00
radekzien <radoslaw.zien@outlook.com>
2025-11-14 12:37:54 +0000

    Refactored and added comments to ChromaExtraction.py

--- ChromaExtraction.py
+++ ChromaExtraction.py
@@ -2,12 +2,11 @@ import numpy
 
 
 def SplitToWindows(audio_data_resampled, sample_rate, window_size):
-    samples_per_window = int(sample_rate * (window_size / 1000))
-    total_samples = len(audio_data_resampled)
-    num_of_windows = int(total_samples // samples_per_window)
+    samples_per_window = int(sample_rate * (window_size / 1000))#integer for slicing
+    total_samples = len(audio_data_resampled) #Total amount of samples in data
+    num_of_windows = total_samples // samples_per_window
 
-    x = num_of_windows * samples_per_window
-    truncated_audio_data = audio_data_resampled[0:x]
+    truncated_audio_data = audio_data_resampled[0:num_of_windows * samples_per_window] #Only keep full windows
     windows = numpy.reshape(truncated_audio_data, (num_of_windows, samples_per_window))
 
     return windows
