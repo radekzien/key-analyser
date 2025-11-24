7ea304242eaa4e3629576f77e96d729ff3a56d4d
radekzien <radoslaw.zien@outlook.com>
2025-11-24 15:20:50 +0000

    Added FFT unit tests

---
+++ tests/test_FFT.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from ChromaExtraction import FastFourierTransform
+
+def test_fft_empty():
+    result = FastFourierTransform(np.array([]))
+    expected = np.array([], dtype=complex)
+    assert_array_almost_equal(result, expected)
+
+def test_fft_single_element():
+    result = FastFourierTransform(np.array([5+2j]))
+    expected = np.array([5+2j])
+    assert_array_almost_equal(result, expected)
+
+def test_fft_real_input():
+    x = np.array([1, 2, 3, 4], dtype=float)
+    result = FastFourierTransform(x)
+    expected = np.fft.fft(x)
+    assert_array_almost_equal(result, expected)
