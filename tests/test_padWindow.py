7b443086d5fc41cacda8d73385b628dab48ca2d0
radekzien <radoslaw.zien@outlook.com>
2025-11-24 15:11:50 +0000

    Added unit test for padWindow

---
+++ tests/test_padWindow.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import numpy as np
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from ChromaExtraction import padWindow
+
+def test_padWindow():
+    fakeWindow = np.ones(5)
+    result = padWindow(fakeWindow)
+
+    expected = np.array([1,1,1,1,1,0,0,0], dtype=complex) #Padded with zero to size 8 as 8 is a power of 2
+    assert np.array_equal(result, expected)
