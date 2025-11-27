9e950cdeebe9b1b0d38c5d7d058bf47a436b9ea1
radekzien <radoslaw.zien@outlook.com>
2025-11-27 13:07:44 +0000

    Optimised FFT function; Changed numpy import

--- ChromaExtraction.py
+++ ChromaExtraction.py
@@ -1,4 +1,4 @@
-from numpy import reshape, zeros, ceil, log2, array, cos, pi, real, imag, sqrt
+import numpy as np
 from cmath import exp
 
 """
@@ -16,7 +16,7 @@ def SplitToWindows(audio_data_resampled, sample_rate, window_size):
     num_of_windows = total_samples // samples_per_window
 
     truncated_audio_data = audio_data_resampled[0:num_of_windows * samples_per_window] #Only keep full windows
-    windows = reshape(truncated_audio_data, (num_of_windows, samples_per_window))
+    windows = np.reshape(truncated_audio_data, (num_of_windows, samples_per_window))
 
     return windows
 
@@ -25,13 +25,13 @@ Pads windows so that they are a size = 2^n
 This is to ensure the recursive step in FFT executes correctly
 """
 def padWindow(window):
-    window = array(window, dtype=complex)
+    window = np.array(window, dtype=complex)
     N_orig = len(window)
         
-    N = 2**int(ceil(log2(N_orig)))
+    N = 2**int(np.ceil(np.log2(N_orig)))
 
     if N > N_orig:
-        window_padded = zeros(N, dtype=complex)
+        window_padded = np.zeros(N, dtype=complex)
         window_padded[:N_orig] = window
     else:
         window_padded = window.copy()
@@ -44,19 +44,20 @@ Performs Cooley-Tukey 2-radix FFT to produce a frequency bin on each window repr
 """
 def FastFourierTransform(window):
     if window.size == 0:
-        return array([], dtype=complex)
+        return np.array([], dtype=complex)
     if window.size == 1:
         return window
+
+    N = len(window)
 
     even = FastFourierTransform(window[::2])
     odd  = FastFourierTransform(window[1::2])
 
-    Y = zeros(len(window), dtype=complex)
-    for k in range(len(window)//2):
-        twiddle = exp(-2j * pi * k / len(window))
-        Y[k] = even[k] + twiddle * odd[k]
-        Y[k + len(window)//2] = even[k] - twiddle * odd[k]
+    Y = np.zeros(N, dtype=complex)
+    twiddle = np.exp(-2j * np.pi * np.arange(N//2) / N)
 
+    Y[:N//2] = even + twiddle * odd
+    Y[N//2:] = even - twiddle * odd
     return Y
 
 """
@@ -68,10 +69,10 @@ def FrequencyAndMagnitude(Y):
     Upper = N//2
     mf =[]
     for n in range(0,Upper):
-        Re = real(Y[n])
-        Im = imag(Y[n])
+        Re = np.real(Y[n])
+        Im = np.imag(Y[n])
 
-        magnitude = sqrt((Re**2)+(Im**2))
+        magnitude = np.sqrt((Re**2)+(Im**2))
         frequency = (n/1024)*16000
 
         mf.append([frequency,magnitude])
@@ -83,12 +84,12 @@ Calculates the chroma bins by turning Frequencies into MIDI values and summing t
 across each window
 """
 def ChromaBins(mf):
-    chroma = zeros(12)
+    chroma = np.zeros(12)
 
     for n in range(0, len(mf)):
         if(mf[n][0] <= 0):
             continue
-        midi = 69 + (12 * log2(mf[n][0]/440))
+        midi = 69 + (12 * np.log2(mf[n][0]/440))
         bin = round(midi) % 12
         chroma[bin] = chroma[bin] + mf[n][1]
     
@@ -98,7 +99,7 @@ def ChromaBins(mf):
 Averages all Chroma Bins across each window into one global chroma feature
 """
 def ExtractChroma(chroma_bins):
-    chroma_feature = zeros(12)
+    chroma_feature = np.zeros(12)
 
     for chroma_vec in chroma_bins:
         chroma_feature += chroma_vec
