21c1fb1b52e3fdb4cd01bc578bcf494db37efed8
radekzien <radoslaw.zien@outlook.com>
2025-11-28 16:20:51 +0000

    Added comments to FreqAndmag test

--- tests/test_FreqAndMag.py
+++ tests/test_FreqAndMag.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from ChromaExtraction import FrequencyAndMagnitude
 
+"""Correctly detects frequency of a known sine wave"""
 def test_FreqAndMag():
     fs = 16000
     N = 1024
